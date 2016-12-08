52e017f4b0ac631b4e5e31757f03cfb2c108cb2a
ganben <caozhao@gmail.com>
2016-12-09 02:44:03 +0800

    :fire:

--- py/server.py
+++ py/server.py
@@ -5,51 +5,53 @@ import socket
 import sys
 import time
 import struct
+#from SocketServer import *
 import SocketServer
-HOST = None               # Symbolic name meaning all available interfaces
+#import SocketServer.BaseRequestHandler
+HOST = '0.0.0.0'               # Symbolic name meaning all available interfaces
 PORT = 8555              # Arbitrary non-privileged port
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except socket.error as msg:
-        s = None
-        continue
-    try:
-        s.bind(sa)
-        s.listen(10)
-    except socket.error as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print 'could not open socket'
-    sys.exit(1)
+#s = None
+#for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
+#                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+#    af, socktype, proto, canonname, sa = res
+#    try:
+#        s = socket.socket(af, socktype, proto)
+#    except socket.error as msg:
+#        s = None
+#        continue
+#    try:
+#        s.bind(sa)
+#        s.listen(10)
+#    except socket.error as msg:
+#        s.close()
+#        s = None
+#        continue
+#    break
+#if s is None:
+#    print 'could not open socket'
+#    sys.exit(1)
 #global conn
 #global addr
-def SocketServer():
-    try:
-        while True:
-            conn, addr = s.accept()
-            print 'Connected by', addr
+#def sserv():
+#    try:
+#        while True:
+#            conn, addr = s.accept()
+#            print 'Connected by', addr
 #while 1:
-            while True:
-                data = conn.recv(1024)
+           # while True:
+#                data = conn.recv(1024)
     #if data:
-                if not data: break
+#                if not data: break
     #conn.send(data)
-                arrs = []
-                for e in data:
-                    arrs.append(str(struct.unpack('B', e[0])[0]))
-                print('-'.join(arrs))
+#                arrs = []
+#                for e in data:
+#                    arrs.append(str(struct.unpack('B', e[0])[0]))
+#                print('-'.join(arrs))
     #else:
-            conn.close()
+#            conn.close()
     #conn, addr = s.accept()
-    except Exception,ex:
-        print ex
+#    except Exception,ex:
+#        print ex
     #print('%s' % data)
 #conn.close()
 #SocketServer()i
@@ -66,6 +68,10 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print "{} wrote:".format(self.client_address[0])
+        arrs = []
+        for e in self.data:
+            arrs.append(str(struct.unpack('B', e[0])[0]))
+        print('-'.join(arrs))
 
 if __name__ == "__main__":
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
