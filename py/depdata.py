592fb5c06ce3ac02727dec140aac52a5936439e1
ganben <caozhao@gmail.com>
2016-12-27 11:43:30 +0800

    :construction:

--- py/depdata.py
+++ py/depdata.py
@@ -3,6 +3,15 @@
 
 import uuid, time, socket, fcntl
 #for json type data use : inbetween
+BCTYPE00 = 'b00' 
+BCTYPE01 = 'b01'
+#static value of bracelet type
+KEY_HEARTBEAT = '01'
+KEY_ALARM = '02'
+KEY_BINDING = '03'
+KEY_LOST = '04'
+#static value 4 bracelet state, from proto document
+
 def get_mac_address_full():
     mac=uuid.UUID(int = uuid.getnode()).hex[-12:]
     return ':'.join([mac[e:e+2] for e in range(0,11,2)])
@@ -25,12 +34,30 @@ def alarm_update(bcid, flag):
         print('alarmtimes=%s' % alarm_cache[bc])
         return False
 
-def rawdata_translate(rawbin):
+def rawdata_translate(manufdata):
     #this is 4 raw data collect and return a expected data prot
     #as demanded type
-    
+    flagdict = {}
+    bctype = manu_filter(manufdata)
+    if bctype == BCTYPE00:
+        keyflag = manufdata[6:8]
+        if keyflag == '13':
+            flagdict['key'] = KEY_HEARTBEAT
+        elif: keyflag == '66':
+            flagdict['key'] = KEY_ALARM
+        #the alarm update count in later 
+    elif bctype == BCTYPE01:
+        statflag = manufdata[6:8]
+        keyflag = manufdata[8:10]
+        if keyflag == '80':
+            flagdict['key'] = KEY_ALARM
+        elif keyflag == '00' and statflag == '14' :
+            flagdict['key'] = KEY_LOST
+        else:
+            flagdict['key'] = KEY_HEARTBEAT            
+        #the alarm update count in later phase
 
-    return resdict
+    return flagdict
 
 def gen_bin_data(datadict, bcmanu):
     #this generate same bin data type 4 two bc adv version
@@ -43,9 +70,16 @@ def gen_json_data(datadict, bcmanu):
     return json
 
 def manu_filter(manudatabin):
-    ##
+    ##filter only start with 00ff12 4#bc1.0
+    ##filter only start with 01ff12 4#sbc1.21
+    if manudatabin == None:
+        return False
 
-    return True #if matches
-
-    return False #if not matches supported bc adv packet
+    if manudatabin.startswith(braceletFlag):
+        return BCTYPE00
+    #if matches, return bcmanu = b00/b01
+    elif manudatabin.startswith('01ff12'):
+        return BCTYPE01
+    else:
+        return False #if not matches supported bc adv packet
 
